f25ebbb8d9a4ffa023b0ce693f26bf012d804945
Shivam Singh <siitubuttus@gmail.com>
2025-08-03 15:07:41 +0530

    Fixed clean server.js

--- server.js
+++ server.js
@@ -6,46 +6,29 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-// ====== DATABASE CONNECTION ======
-mongoose.connect(mongodb+srv://sittubittus:<db_W6PXYmt8tefZ7f4q>@cluster0.ss60ylm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("mongodb+srv://sittubittus:<db_W6PXYmt8tefZ7f4q>@cluster0.ss60ylm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-// ====== SCHEMA ======
-const UserSchema = new mongoose.Schema({
+const User = mongoose.model("User", new mongoose.Schema({
   name: String,
   email: String,
   password: String
-});
-const User = mongoose.model("User", UserSchema);
+}));
 
-// ====== REAL API ROUTES ======
 app.post("/api/register", async (req, res) => {
-  const { name, email, password } = req.body;
-  const user = new User({ name, email, password });
+  const user = new User(req.body);
   await user.save();
-  res.json({ success: true, message: "User registered" });
+  res.json({ success: true });
 });
 
 app.post("/api/login", async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email, password });
-  if (user) {
-    res.json({ success: true, message: "Login successful" });
-  } else {
-    res.json({ success: false, message: "Invalid credentials" });
-  }
+  const user = await User.findOne({ email: req.body.email, password: req.body.password });
+  res.json({ success: !!user });
 });
 
-app.get("/api/users", async (req, res) => {
-  const users = await User.find();
-  res.json(users);
-});
+app.get("/api/users", async (req, res) => res.json(await User.find()));
 
-// ====== DEFAULT ROUTE ======
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "public", "index.html")));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(\`Server running on port \${PORT}\`));
+app.listen(process.env.PORT || 3000, () => console.log("Server running"));
