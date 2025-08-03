68d0a38f1ce91141020ed85d3ca2e2acc02efc40
Shivam Singh <siitubuttus@gmail.com>
2025-08-03 19:37:54 +0530

    Google-style login with animations, fixed images

--- public/script.js
+++ public/script.js
@@ -1,12 +1,12 @@
 const bundles = [
-  { img:"https://i.imgur.com/3O6HhQf.jpg", name:"Judgement King + War Machine Scenery", desc:"November Gold Pass Bundle", link:"/login.html" },
-  { img:"https://i.imgur.com/3yJeuyQ.jpg", name:"Anime Royal Champion + Anime Scenery", desc:"August Gold Pass Bundle", link:"/login.html" },
-  { img:"https://i.imgur.com/2rN3Qn3.jpg", name:"Shadow Scenery", desc:"Dark & mysterious base design", link:"/login.html" },
-  { img:"https://i.imgur.com/QnP8bd4.jpg", name:"Space Scenery", desc:"Futuristic galaxy theme base", link:"/login.html" },
-  { img:"https://i.imgur.com/FJJkXCM.jpg", name:"Epic Jungle Scenery", desc:"Lush and adventurous theme", link:"/login.html" },
-  { img:"https://i.imgur.com/7JQhL7q.jpg", name:"Pirate Champion + Pirate Scenery", desc:"Arrr! Clash on the high seas", link:"/login.html" },
-  { img:"https://i.imgur.com/xWxElUk.jpg", name:"Winter Queen + Snowy Scenery", desc:"Festive holiday themed bundle", link:"/login.html" },
-  { img:"https://i.imgur.com/ULuCwuk.jpg", name:"Legendary Scenery", desc:"The rarest base theme yet", link:"/login.html" }
+  { img:"https://static.wikia.nocookie.net/clashofclans/images/6/6d/Judgement_King_Skin.png", name:"Judgement King + War Machine Scenery", desc:"November Gold Pass Bundle", link:"/login.html" },
+  { img:"https://static.wikia.nocookie.net/clashofclans/images/0/0f/Anime_Royal_Champion_Skin.png", name:"Anime Royal Champion + Anime Scenery", desc:"August Gold Pass Bundle", link:"/login.html" },
+  { img:"https://static.wikia.nocookie.net/clashofclans/images/2/20/Shadow_Scenery.png", name:"Shadow Scenery", desc:"Dark & mysterious base design", link:"/login.html" },
+  { img:"https://static.wikia.nocookie.net/clashofclans/images/1/1d/Space_Scenery.png", name:"Space Scenery", desc:"Futuristic galaxy theme base", link:"/login.html" },
+  { img:"https://static.wikia.nocookie.net/clashofclans/images/e/e1/Jungle_Scenery.png", name:"Epic Jungle Scenery", desc:"Lush and adventurous theme", link:"/login.html" },
+  { img:"https://static.wikia.nocookie.net/clashofclans/images/a/a2/Pirate_Champion_Skin.png", name:"Pirate Champion + Pirate Scenery", desc:"Arrr! Clash on the high seas", link:"/login.html" },
+  { img:"https://static.wikia.nocookie.net/clashofclans/images/7/75/Winter_Queen_Skin.png", name:"Winter Queen + Snowy Scenery", desc:"Festive holiday themed bundle", link:"/login.html" },
+  { img:"https://static.wikia.nocookie.net/clashofclans/images/3/36/Legendary_Scenery.png", name:"Legendary Scenery", desc:"The rarest base theme yet", link:"/login.html" }
 ];
 const container = document.querySelector('.offers');
 bundles.forEach(b => {
