8ea305de7780a6ba4bfc34c4a39f73684183cea2
Shivam Singh <siitubuttus@gmail.com>
2025-08-03 20:30:49 +0530

    Rebuilt site: 4-column grid, Google OAuth modal, fixed redirect

--- public/script.js
+++ public/script.js
@@ -1,12 +1,12 @@
 const bundles = [
-  { img:"https://cdn.supercell.com/supercell.com/21082023/skins/judgement_king.png", name:"Judgement King + War Machine Scenery", desc:"November Gold Pass Bundle" },
-  { img:"https://cdn.supercell.com/supercell.com/21082023/skins/anime_royal_champion.png", name:"Anime Royal Champion + Anime Scenery", desc:"August Gold Pass Bundle" },
-  { img:"https://cdn.supercell.com/supercell.com/21082023/sceneries/shadow_scenery.png", name:"Shadow Scenery", desc:"Dark & mysterious base design" },
-  { img:"https://cdn.supercell.com/supercell.com/21082023/sceneries/space_scenery.png", name:"Space Scenery", desc:"Futuristic galaxy theme base" },
-  { img:"https://cdn.supercell.com/supercell.com/21082023/sceneries/jungle_scenery.png", name:"Epic Jungle Scenery", desc:"Lush and adventurous theme" },
-  { img:"https://cdn.supercell.com/supercell.com/21082023/skins/pirate_champion.png", name:"Pirate Champion + Pirate Scenery", desc:"Arrr! Clash on the high seas" },
-  { img:"https://cdn.supercell.com/supercell.com/21082023/skins/winter_queen.png", name:"Winter Queen + Snowy Scenery", desc:"Festive holiday themed bundle" },
-  { img:"https://cdn.supercell.com/supercell.com/21082023/sceneries/legendary_scenery.png", name:"Legendary Scenery", desc:"The rarest base theme yet" }
+  { img:"https://cdn.supercell.com/supercell.com/21082023/skins/judgement_king.png", name:"Judgement King Bundle", desc:"November Gold Pass Exclusive" },
+  { img:"https://cdn.supercell.com/supercell.com/21082023/skins/anime_royal_champion.png", name:"Anime Royal Champion", desc:"Anime-themed Royal Champion skin" },
+  { img:"https://cdn.supercell.com/supercell.com/21082023/sceneries/shadow_scenery.png", name:"Shadow Scenery", desc:"Dark mysterious village layout" },
+  { img:"https://cdn.supercell.com/supercell.com/21082023/sceneries/space_scenery.png", name:"Space Scenery", desc:"Clash in the cosmos!" },
+  { img:"https://cdn.supercell.com/supercell.com/21082023/sceneries/jungle_scenery.png", name:"Jungle Scenery", desc:"Adventure in the wild jungle" },
+  { img:"https://cdn.supercell.com/supercell.com/21082023/skins/pirate_champion.png", name:"Pirate Champion Bundle", desc:"Set sail for victory!" },
+  { img:"https://cdn.supercell.com/supercell.com/21082023/skins/winter_queen.png", name:"Winter Queen", desc:"Freeze your enemies in style" },
+  { img:"https://cdn.supercell.com/supercell.com/21082023/sceneries/legendary_scenery.png", name:"Legendary Scenery", desc:"Rare legendary village theme" }
 ];
 const container = document.querySelector('.offers');
 bundles.forEach(b => {
@@ -14,14 +14,12 @@ bundles.forEach(b => {
   card.className = 'card';
   card.innerHTML = `
     <img src="${b.img}" alt="${b.name}">
-    <div class="card-content">
-      <h3>${b.name}</h3>
-      <p>${b.desc}</p>
-      <button onclick="openLogin()">Claim Now</button>
-    </div>`;
+    <h3>${b.name}</h3>
+    <p>${b.desc}</p>
+    <button onclick="openLogin()">Claim Now</button>
+  `;
   container.appendChild(card);
 });
-
 function openLogin(){
   document.getElementById('loginModal').style.display='flex';
 }
