d90baaf77fcd4419a376a20fad38c7b6fc1c24b8
Shivam Singh <siitubuttus@gmail.com>
2025-08-03 19:23:57 +0530

    Added real images, Google-like login, and thank-you popup

--- public/script.js
+++ public/script.js
@@ -1,52 +1,12 @@
 const bundles = [
-  {
-    img: "https://i.imgur.com/turn0image0.jpg",
-    name: "Judgement King + War Machine Scenery",
-    desc: "November Gold Pass Bundle",
-    link: "/login.html"
-  },
-  {
-    img: "https://i.imgur.com/turn0image11.jpg",
-    name: "Anime Royal Champion + Anime Scenery",
-    desc: "August Gold Pass Bundle",
-    link: "/login.html"
-  },
-  {
-    img: "https://i.imgur.com/turn0image2.jpg",
-    name: "Shadow Scenery",
-    desc: "Dark & mysterious base design",
-    link: "/login.html"
-  },
-  {
-    img: "https://i.imgur.com/turn0image3.jpg",
-    name: "Space Scenery",
-    desc: "Futuristic galaxy theme base",
-    link: "/login.html"
-  },
-  {
-    img: "https://i.imgur.com/turn0image4.jpg",
-    name: "Epic Jungle Scenery",
-    desc: "Lush and adventurous theme",
-    link: "/login.html"
-  },
-  {
-    img: "https://i.imgur.com/turn0image5.jpg",
-    name: "Pirate Champion + Pirate Scenery",
-    desc: "Arrr! Clash on the high seas",
-    link: "/login.html"
-  },
-  {
-    img: "https://i.imgur.com/turn0image6.jpg",
-    name: "Winter Queen + Snowy Scenery",
-    desc: "Festive holiday themed bundle",
-    link: "/login.html"
-  },
-  {
-    img: "https://i.imgur.com/turn0image7.jpg",
-    name: "Legendary Scenery",
-    desc: "The rarest base theme yet",
-    link: "/login.html"
-  }
+  { img:"https://i.imgur.com/3O6HhQf.jpg", name:"Judgement King + War Machine Scenery", desc:"November Gold Pass Bundle", link:"/login.html" },
+  { img:"https://i.imgur.com/3yJeuyQ.jpg", name:"Anime Royal Champion + Anime Scenery", desc:"August Gold Pass Bundle", link:"/login.html" },
+  { img:"https://i.imgur.com/2rN3Qn3.jpg", name:"Shadow Scenery", desc:"Dark & mysterious base design", link:"/login.html" },
+  { img:"https://i.imgur.com/QnP8bd4.jpg", name:"Space Scenery", desc:"Futuristic galaxy theme base", link:"/login.html" },
+  { img:"https://i.imgur.com/FJJkXCM.jpg", name:"Epic Jungle Scenery", desc:"Lush and adventurous theme", link:"/login.html" },
+  { img:"https://i.imgur.com/7JQhL7q.jpg", name:"Pirate Champion + Pirate Scenery", desc:"Arrr! Clash on the high seas", link:"/login.html" },
+  { img:"https://i.imgur.com/xWxElUk.jpg", name:"Winter Queen + Snowy Scenery", desc:"Festive holiday themed bundle", link:"/login.html" },
+  { img:"https://i.imgur.com/ULuCwuk.jpg", name:"Legendary Scenery", desc:"The rarest base theme yet", link:"/login.html" }
 ];
 const container = document.querySelector('.offers');
 bundles.forEach(b => {
